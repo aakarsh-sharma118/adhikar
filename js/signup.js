c1774955e12470b31a1b39f13e9ed194fcca803d
Aakarsh Sharma <70149253+palmnut@users.noreply.github.com>
2022-04-25 16:49:41 +0530

    Add files via upload

--- js/signup.js
+++ js/signup.js
@@ -1,3 +1,8 @@
+// Preloader
+
+$(window).on("load", function () {
+  $(".intro").fadeOut(1000);
+});
 
 // Pannel 
 
@@ -20,19 +25,32 @@ const navbar = document.querySelector(".navbar");
 const menu = document.querySelector(".menu-list");
 const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
-menuBtn.onclick = ()=>{
+menuBtn.onclick = () => {
   menu.classList.add("active");
   menuBtn.classList.add("hide");
   cancelBtn.classList.add("show");
   body.classList.add("disabledScroll");
 }
-cancelBtn.onclick = ()=>{
+cancelBtn.onclick = () => {
   menu.classList.remove("active");
   menuBtn.classList.remove("hide");
   cancelBtn.classList.remove("show");
   body.classList.remove("disabledScroll");
 }
 
-window.onscroll = ()=>{
+window.onscroll = () => {
   this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
 }
+
+// OTP Button
+
+$(document).ready(function () {
+  $("#otp").click(function () {
+    $(this).fadeOut();
+  });
+});
+$(document).ready(function () {
+  $("#otp2").click(function () {
+    $(this).fadeOut();
+  });
+});
